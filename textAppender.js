1e4866bfdbaae49ce659821ab74ccabcc0eec383
Andreas Cederström <andreas.cederstrom@gmail.com>
2018-04-03 23:40:10 +0200

    Smoother positioning of text

--- textAppender.js
+++ textAppender.js
@@ -1,3 +1,8 @@
+var marginTop = 100;
+var maxWith = 300;
+var text1MarginLeft = 20;
+var text2MarginLeft = 360;
+
 var sourceImg = new Image();
 sourceImg.src = 'source.png';
 
@@ -5,15 +10,20 @@ var text1Elem = document.getElementById('text1');
 var text2Elem = document.getElementById('text2');
 
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext('2d');
+ctx.font = 'normal 30px arial';
 
 function drawCanvas() {
+    ctx.drawImage(sourceImg, 0, 0);
     var text1 = text1Elem.value;
     var text2 = text2Elem.value;
-    ctx.drawImage(sourceImg, 0, 0);
-    ctx.font = 'normal 30px arial';
-    ctx.fillText(text1, 20, 100, 300);
-    ctx.fillText(text2, 360, 100, 300);
+    ctx.fillText(text1, text1MarginLeft + calcOffset(text1, .5, 8), marginTop, maxWith);
+    ctx.fillText(text2, text2MarginLeft + calcOffset(text2, .2, 4), marginTop, maxWith);
+}
+
+function calcOffset(text, whereToStart, charWidth) {
+    var offset = (maxWith * whereToStart) - (text.length * charWidth);
+    return offset > 0 ? offset : 0;
 }
 
 text1Elem.addEventListener('input', drawCanvas);
