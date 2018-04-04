b33526ac51d75550224113dd7360cde520a75398
Andreas Cederström <andreas.cederstrom@gmail.com>
2018-04-04 21:49:26 +0200

    No need for the canvas element in HTML anymore

--- textAppender.js
+++ textAppender.js
@@ -2,14 +2,19 @@ var marginTop = 100;
 var maxWith = 300;
 var text1MarginLeft = 20;
 var text2MarginLeft = 360;
+var canvasWidth = canvasHeight = 680;
 
 var sourceImg = new Image();
-sourceImg.src = 'source.png';
+sourceImg.src = 'https://cederstrom.github.io/den-ar-efterbliven/source.png';
+sourceImg.crossOrigin = "Anonymous";
+var outImg = document.getElementById('outImg');
 
 var text1Elem = document.getElementById('text1');
 var text2Elem = document.getElementById('text2');
 
-var canvas = document.getElementById('canvas');
+var canvas = document.createElement('canvas');
+canvas.height = canvasHeight;
+canvas.width = canvasWidth;
 var ctx = canvas.getContext('2d');
 ctx.font = 'normal 30px arial';
 
@@ -19,6 +24,7 @@ function drawCanvas() {
     var text2 = text2Elem.value;
     ctx.fillText(text1, text1MarginLeft + calcOffset(text1, .5, 8), marginTop, maxWith);
     ctx.fillText(text2, text2MarginLeft + calcOffset(text2, .2, 4), marginTop, maxWith);
+    outImg.src = canvas.toDataURL();
 }
 
 function calcOffset(text, whereToStart, charWidth) {
