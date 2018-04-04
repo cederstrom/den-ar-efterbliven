0b423839d27ffda7ac78c60f76f97c94526531bc
Andreas Cederström <andreas.cederstrom@gmail.com>
2018-04-04 23:16:40 +0200

    Added link to page with static copy of image

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var text1Elem = document.getElementById('text1');
 var text2Elem = document.getElementById('text2');
 var outImg = document.getElementById('outImg');
+var link = document.getElementById('link');
 
 function onChangeListener() {
-    var canvas = drawCanvas(text1Elem.value, text2Elem.value);
+    var text1 = text1Elem.value;
+    var text2 = text2Elem.value;
+    var canvas = drawCanvas(text1, text2);
     outImg.src = canvas.toDataURL();
+    link.href = 'generated.html#' + encodeURI(text1) + '/' + encodeURI(text2);
 }
 
 text1Elem.addEventListener('input', onChangeListener);
